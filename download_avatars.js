add335167deda8fd267213122ae1866290885529
Nina Yujiri <ninayujiri@gmail.com>
2018-04-16 23:08:35 +0000

    added headers

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,19 @@
-//Using the request package, you will fetch the list of contributors as a JSON string
-//from the GitHub API's contributors endpoint -> https://api.github.com/repos/jquery/jquery/contributors
-//Upon receiving the results, your function will invoke a callback function with the results.
-//This callback function will loop over and print out the avatar_url for each object in the collection.
-
-
 var request = require('request');
+var secrets = require('./secrets')
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
 
-// fetches the list of contributors as a JSON string
 function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets
+    }
+  };
 
-
-  //receives results
-
-  //invokes a callback function with the results
-
-  //callback function loops over and prints out the avatar_url for each object
-
-
-
-
+  request(options, function(err, res, body) {
+    callback(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
