cd016c9f2a9f937a8a0d0f335f3a278a0e380a26
Nina Yujiri <ninayujiri@gmail.com>
2018-04-17 01:50:12 +0000

    added error if second arguement is empty

--- download_avatars.js
+++ download_avatars.js
@@ -5,31 +5,34 @@ var secrets = require('./secrets');
 
 
 function getRepoContributors(repoOwner, repoName, callback) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': secrets
+  if (repoOwner == null || repoName == null) {
+    console.log("Error! Pass a second arguement.")
+  } else {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': secrets
+      }
     }
+
+    request(options, function(err, res, body) {
+      if (err) {
+        throw err;
+      }
+
+      var parsed = JSON.parse(body);
+
+      var contributors = [];
+
+      for(var i = 0; i < parsed.length; i++) {
+        var avatarLink = parsed[i].avatar_url;
+        contributors.push({url: avatarLink, fileName: parsed[i].login});
+      }
+
+      callback(err, contributors);
+    });
   }
-
-  request(options, function(err, res, body) {
-    if (err) {
-      throw err;
-    }
-
-    var parsed = JSON.parse(body);
-
-    var contributors = [];
-
-    for(var i = 0; i < parsed.length; i++) {
-      var avatarLink = parsed[i].avatar_url;
-      contributors.push({url: avatarLink, fileName: parsed[i].login});
-    }
-
-    callback(err, contributors);
-
-  });
 }
 
 getRepoContributors(process.argv[2], process.argv[3], downloadImageByURL);
